9981af93b623deb2de8e8c8e502a4c358c9ad7b4
rajneeshNadcab <rajneeshNadcab@gmail.com>
2022-05-13 17:02:21 +0530

    first commit

--- wp-includes/js/jquery/jquery.minaf6c.js
+++ wp-includes/js/jquery/jquery.minaf6c.js
@@ -1,6 +1,8 @@
 /*! jQuery v3.6.0 | (c) OpenJS Foundation and other contributors | jquery.org/license */
+
 !(function (e, t) {
   "use strict";
+  
   "object" == typeof module && "object" == typeof module.exports
     ? (module.exports = e.document
         ? t(e, !0)
@@ -5572,4 +5574,5 @@
     S
   );
 });
+
 jQuery.noConflict();
